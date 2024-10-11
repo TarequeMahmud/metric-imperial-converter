561ceab51022ff205a30a88f1dd408cf2ffb6aec
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-11 20:00:57 +0600

    Initial conversion api setup

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
-
+  app.get("/api/convert", (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getString(input);
+    const returnUnit = convertHandler.returnUnit(initUnit);
+    const spellOutUnit = convertHandler.spellOutUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const stringToReturn = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+  });
 };
