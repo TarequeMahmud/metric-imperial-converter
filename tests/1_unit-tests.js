bdf0c3a2aa86c275452df5d75f33fc830dde63a2
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-12 15:46:12 +0600

    fix: setting up unit tests correctly and fixing errors

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,42 +4,95 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 const input = process.env.input;
-const initNum = convertHandler.getNum(input);
-const initUnit = convertHandler.getUnit(input);
-const returnUnit = convertHandler.getReturnUnit(initUnit);
-const initSpellOutUnit = convertHandler.spellOutUnit(initUnit);
-const returnSpellOutUnit = convertHandler.spellOutUnit(returnUnit);
-const returnNum = convertHandler.convert(initNum, initUnit);
-const stringToReturn = convertHandler.getString(
-  initNum,
-  initSpellOutUnit,
-  returnNum,
-  returnSpellOutUnit
-);
 
-suite("ConvertHandler", function () {
-  test(".getNum() should return correct number from the input", () => {
-    assert.equal(initNum, process.env.initNum);
+//
+// = convertHandler.spellOutUnit(returnUnit);
+// convertHandler.convert(initNum, initUnit);
+// const stringToReturn = convertHandler.getString(
+//   initNum,
+//   initSpellOutUnit,
+//   returnNum,
+//   returnSpellOutUnit
+// );
+
+suite("Unit Tests", function () {
+  suite("Testing getNum() function", () => {
+    test("convertHandler should correctly read a whole number input.", (done) => {
+      const initNum = "5L";
+      assert.equal(convertHandler.getNum(initNum), 5);
+      done();
+    });
+    test("convertHandler should correctly read a decimal input.", (done) => {
+      const initNum = "5.2L";
+      assert.equal(convertHandler.getNum(initNum), 5.2);
+      done();
+    });
+    test("convertHandler should correctly read a fractional input.", (done) => {
+      const initNum = "5/2L";
+      assert.equal(convertHandler.getNum(initNum), 5 / 2);
+      done();
+    });
+    test("convertHandler should correctly read a fractional input with a decimal.", (done) => {
+      const initNum = "5.1/2L";
+      assert.equal(convertHandler.getNum(initNum), 2.55);
+      done();
+    });
+    test("convertHandler should correctly return an error on a double-fraction", (done) => {
+      const initNum = "5/1/2L";
+      assert.equal(convertHandler.getNum(initNum), null);
+      done();
+    });
+    test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided", (done) => {
+      const initNum = "L";
+      assert.equal(convertHandler.getNum(initNum), 1);
+      done();
+    });
   });
 
-  test(".getUnit() should return the correct unit output", () => {
-    assert.equal(initUnit, process.env.initUnit);
+  suite("Testing getUnit", () => {
+    test("convertHandler should correctly read each valid input unit.", () => {
+      const initNum = "5L";
+      assert.equal(convertHandler.getUnit(initNum), "L");
+    });
+
+    test("convertHandler should correctly return an error for an invalid input unit.", () => {
+      const initNum = "5a";
+      assert.equal(convertHandler.getUnit(initNum), null);
+    });
+
+    test("convertHandler should return the correct return unit for each valid input unit.", () => {
+      const initUnit = "L";
+      assert.equal(convertHandler.getReturnUnit(initUnit), "gal");
+    });
+    test("convertHandler should correctly return the spelled-out string unit for each valid input unit.", () => {
+      const initUnit = "L";
+      assert.equal(convertHandler.spellOutUnit(initUnit), "liters");
+    });
   });
 
-  test(".getReturnUnit() should return the proper return unit", () => {
-    assert.equal(returnUnit, process.env.returnUnit);
+  suite("Testing Conversion abilitiy", () => {
+    test("convertHandler should correctly convert gal to L", () => {
+      assert.isAtLeast(convertHandler.convert(1, "gal"), 3.78541);
+    });
+
+    test("convertHandler should correctly convert L to gal", () => {
+      assert.isAtLeast(convertHandler.convert(3.78541, "L"), 1);
+    });
+    test("convertHandler should correctly convert mi to km", () => {
+      assert.isAtLeast(convertHandler.convert(1, "mi"), 1.60934);
+    });
+    test("convertHandler should correctly convert km to mi", () => {
+      assert.isAtLeast(convertHandler.convert(1.60934, "km"), 1);
+    });
+    test("convertHandler should correctly convert lbs to kg", () => {
+      assert.isAtLeast(convertHandler.convert(1, "lbs"), 0.45359);
+    });
+    test("convertHandler should correctly convert kg to lb", () => {
+      assert.isAtLeast(convertHandler.convert(0.45359, "kg"), 1);
+    });
   });
 
-  test(".spellOutUnit() should return proper spellOutUnit", () => {
-    assert.equal(initSpellOutUnit, process.env.initSpellOutUnit);
-    assert.equal(returnSpellOutUnit, process.env.returnSpellOutUnit);
-  });
-
-  test(".convert() should return proper converted number", () => {
-    assert.isAtLeast(returnNum, Number(process.env.returnNum));
-  });
-
-  test(".getString() should return proper string to return", () => {
-    assert.equal(stringToReturn, process.env.stringToReturn);
-  });
+  // test(".getString() should return proper string to return", () => {
+  //   assert.equal(stringToReturn, process.env.stringToReturn);
+  // });
 });
